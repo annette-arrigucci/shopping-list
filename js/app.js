06ed4d2ec9b586724bf757dfcfff3697475005fa
Annette Arrigucci <annette.arrigucci@gmail.com>
2016-06-21 17:35:14 -0400

    functional

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
 	$("#add-item-button").click( function () {
-		var mystring = "<li><input type='checkbox' id='new-needed-item' class='css-checkbox lrg'><label for='new-needed-item' name='new-needed-item_lbl' class='css-label lrg vlad'>" + $("input:text").val() + "</label><button type='button'>x</button></li>";
+		var itemname = $("input:text").val();
+		var mystring = "<li><input type='checkbox' id='" + itemname + "' class='css-checkbox lrg'><label for='" + itemname +"' name='"+ itemname +"_lbl' class='css-label lrg vlad'>" + itemname + "</label><button type='button' class='delete-button'>x</button></li>";
 		$(".needed-items").prepend(mystring);
 		$('input:text').val('');
-		//alert($("input:text").val());
 	})
     $('ul').on('click', '.css-checkbox', function(event){
-    	//if($(".css-checkbox").prop("checked")==true)
-    	//if(this.checked) == true
+    	
     	if ($(this).is(':checked'))
     	{
     		$(".checked-off-items").prepend($(this).parents("li"));
-    			//$(this).parents("li").hide();
     	}
     	else if(!$(this).is(':checked')) {
     		$(".needed-items").prepend($(this).parents("li"));
     	}
-  // fires when any LIs are clicked on
-  // including LIs that aren't on the page when it is initially loaded
+    })
+    $('ul').on('click', '.delete-button', function(event){
+    	$(this).parents("li").remove();
     });
 });
